0874bb589411c80c6de617c02ed570d3d5ce20f5
Allan Kerr <personal.allankerr@gmail.com>
2017-01-19 21:43:55 -0600

    Added unit tests for the user API submit concern and retract concern end points during peer programmign session with Daniel and Brandon.

--- LTCSafety/src/main/java/com/cs371group2/concern/UserApi.java
+++ LTCSafety/src/main/java/com/cs371group2/concern/UserApi.java
@@ -2,7 +2,9 @@ package com.cs371group2.concern;
 
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.response.BadRequestException;
 import com.google.api.server.spi.response.UnauthorizedException;
+import com.googlecode.objectify.NotFoundException;
 
 @Api(name = "userApi",
         canonicalName = "User API",
@@ -11,12 +13,12 @@ import com.google.api.server.spi.response.UnauthorizedException;
 public final class UserApi {
 
     @ApiMethod(name = "submitConcern", path = "/concern/submit")
-    public OwnerToken submitConcern(ConcernData data) {
+    public OwnerToken submitConcern(ConcernData data) throws BadRequestException {
         return null;
     }
 
     @ApiMethod(name = "retractConcern", path = "/concern/retract")
-    public void retractConcern(OwnerToken token) throws UnauthorizedException {
+    public void retractConcern(OwnerToken token) throws UnauthorizedException, NotFoundException {
 
     }
 }
