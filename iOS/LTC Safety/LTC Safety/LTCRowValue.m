e1d8ffe8f96b67b64d7a1ddca0bb3e231f3d43c1
Allan Kerr <personal.allankerr@gmail.com>
2017-02-03 04:45:10 -0600

    Implemented the row value and value selection view model classes to pass their unit tests.

--- iOS/LTC Safety/LTC Safety/LTCRowValue.m
+++ iOS/LTC Safety/LTC Safety/LTCRowValue.m
@@ -19,11 +19,25 @@ NSString *const LTCTitleKey = @"title";
 
 - (instancetype)initWithDictionary:(NSDictionary *)dictionary {
     
-    return nil;
+    NSAssert(dictionary != nil, @"Attempted to create a row value with a null dictionary.");
+    NSAssert([dictionary valueForKey:LTCTitleKey] != nil, @"Row value dictionary did not contain expected key:%@", LTCTitleKey);
+    
+    NSString *tag = [dictionary valueForKey:LTCTitleKey];
+    return (self = [self initWithTag:tag]);
 }
 
 - (instancetype)initWithTag:(NSString *)tag {
     
+    NSAssert(tag != nil, @"Attempted to initialize a row value with a nil tag");
+
+    if (self = [super init]) {
+        self.tag = tag;
+        self.title = NSLocalizedString(self.tag, nil);
+    }
+    NSAssert(self.tag != nil, @"Initialization failed unexpectedly due to nil tag.");
+    NSAssert(self.title != nil, @"Initialization failed unexpectedly due to nil title.");
+    NSAssert1(self != nil, @"Failed to initialize %@", self.class);
+
     return self;
 }
 
--- iOS/LTC Safety/LTC Safety/LTCValueSelectionViewModel.m
+++ iOS/LTC Safety/LTC Safety/LTCValueSelectionViewModel.m
@@ -23,11 +23,58 @@ NSString *const LTCItemsKey = @"items";
 
 + (instancetype)selectionViewModelForFileName:(NSString *)fileName {
     
+    NSAssert(fileName.length > 0, @"Attempted to create a selection from model with an empty file name.");
+    
+    NSString *path = [[NSBundle mainBundle] pathForResource:fileName ofType:@"plist"];
+    
+    NSAssert1(path != nil, @"Failed to find selection view model with file name: %@", fileName);
+    
+    NSDictionary *configDictionary = [NSDictionary dictionaryWithContentsOfFile:path];
+    
+    NSAssert1(path != nil, @"Failed to load dictionary at path: %@", path);
+    
+    NSString *titleDescriptor = [configDictionary valueForKey:LTCModelTitleKey];
+    NSString *otherPromptDescriptor = [configDictionary valueForKey:LTCOtherPromptKey];
+    
+    NSAssert1(titleDescriptor != nil, @"Failed to find title descriptor in: %@", configDictionary);
+    NSAssert1(otherPromptDescriptor != nil, @"Failed to find other prompt descriptor in: %@", configDictionary);
+
+    NSArray *rowDictionaries = [configDictionary valueForKey:LTCItemsKey];
+    
+    NSAssert1(rowDictionaries.count > 0, @"Failed to find row items in: %@", configDictionary);
+    
+    NSMutableArray *mutableRowValues = [NSMutableArray arrayWithCapacity:rowDictionaries.count];
+    
+    for (NSDictionary *dictionary in rowDictionaries) {
+        LTCRowValue *rowValue = [[LTCRowValue alloc] initWithDictionary:dictionary];
+        [mutableRowValues addObject:rowValue];
+    }
+    NSArray *rowValues = [mutableRowValues copy];
+    
+    NSAssert(rowValues.count == rowDictionaries.count, @"Unexpected number of row values created.");
+    
+    return [[self alloc] initWithTitleDescriptor:titleDescriptor otherPromptDescriptor:otherPromptDescriptor rowValues:rowValues];
 }
 
 - (instancetype)initWithTitleDescriptor:(NSString *)titleDescriptor otherPromptDescriptor:(NSString *)otherPromptDescriptor rowValues:(NSArray <LTCRowValue *>*)rowValues {
     
-   
+    NSAssert(titleDescriptor != nil, @"Attempted to initialize a value selection view model with a nil title descriptor.");
+    NSAssert(otherPromptDescriptor != nil, @"Attempted to initialize a value selection view model with a nil other prompt descriptor.");
+    NSAssert(rowValues.count > 0, @"Attempted to initialize a value selection view model with zero row values.");
+
+    if (self = [super init]) {
+        
+        self.title = NSLocalizedString(titleDescriptor, nil);
+        self.otherPrompt = NSLocalizedString(otherPromptDescriptor, nil);
+        
+        self.rowValues = rowValues;
+    }
+    NSAssert1(self != nil, @"Failed to initialize %@", self.class);
+    NSAssert(self.title != nil, @"Value selection view model finished initialization with a nil title.");
+    NSAssert(self.otherPrompt != nil, @"Value selection view model finished initialization with a nil other prompt.");
+    NSAssert(self.rowValues.count > 0, @"Value selection view model finished initialization with no row values.");
+
+    return self;
 }
 
 @end
