46565926eb89a74a37ecd8a0ff6d51f9f59e59a8
bmj920 <bmj920@usask.ca>
2017-02-06 17:42:51 -0600

    Created Account and AccountDao classes
    Accounts hold information related to a user's permission in the system. The AccountDao saves and loads these accounts from our database.

---
+++ LTCSafety/src/main/java/com/cs371group2/client/Account.java
@@ -0,0 +1,8 @@
+package com.cs371group2.client;
+
+/**
+ * Created by Brandon on 2017-02-06.
+ */
+public class Account {
+
+}
---
+++ LTCSafety/src/main/java/com/cs371group2/client/AccountDao.java
@@ -0,0 +1,13 @@
+package com.cs371group2.client;
+
+import com.cs371group2.Dao;
+
+/**
+ * Created by Brandon on 2017-02-06.
+ */
+public class AccountDao extends Dao<Account> {
+
+    public AccountDao() {
+        super(Account.class);
+    }
+}
