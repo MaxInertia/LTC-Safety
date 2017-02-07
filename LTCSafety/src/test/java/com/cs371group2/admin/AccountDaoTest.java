301439453a62546ad674a3e4a7bc9fdda209e514
bmj920 <bmj920@usask.ca>
2017-02-06 20:16:31 -0600

    Created Account and AccountDao testing classes for TDD
    Created testing classes for writing unit-tests as per Test-Driven Design requirements

---
+++ LTCSafety/src/test/java/com/cs371group2/admin/AccountDaoTest.java
@@ -0,0 +1,13 @@
+package com.cs371group2.admin;
+
+/**
+ *  * This class is used to test the Dao class and the AccountDao class. This class tests the base data
+ * access object class due to it being abstract making it difficult to test directly. As a result,
+ * load, save, and delete are tested despite not being overridden by the concern data access
+ * object.
+ *
+ * Created on 2017-02-06.
+ */
+public class AccountDaoTest {
+
+}
---
+++ LTCSafety/src/test/java/com/cs371group2/admin/AccountTest.java
@@ -0,0 +1,10 @@
+package com.cs371group2.admin;
+
+import com.cs371group2.DatastoreTest;
+
+/**
+ * Created by Brandon on 2017-02-06.
+ */
+public class AccountTest extends DatastoreTest {
+
+}
