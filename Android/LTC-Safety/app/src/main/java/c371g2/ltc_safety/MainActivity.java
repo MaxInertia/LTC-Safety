076333806b8b0aca1c88707ccf39d663e2aea3f0
Dorian Thiessen <NonInertial.git@usask.ca>
2017-02-08 02:44:29 -0600

    Initial Android app project upload
    Default Android Studio project with 'empty' main activity. Additionally, Google client API module and dependencies are included.
    
    Current minimum SDK: "API 14: Android 4.0 (IceCreamSandwich)". Thus the app is currently useable on 97.4% of the devices active on the Google Play Store.

---
+++ Android/LTC-Safety/app/src/main/java/c371g2/ltc_safety/MainActivity.java
@@ -0,0 +1,13 @@
+package c371g2.ltc_safety;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
