80fa350a06d8aaa84573c357aa4e35edbd4ecdfd
Niklaas Neijmeijer <nkn565@osmac6.usask.ca>
2017-02-10 16:45:02 -0600

    Adding files missed in last commit

--- iOS/LTC Safety/LTC SafetyUITests/LTC_SafetyUITests.m
+++ iOS/LTC Safety/LTC SafetyUITests/LTC_SafetyUITests.m
@@ -16,16 +16,430 @@
 
 - (void)setUp {
     [super setUp];
+    [[[XCUIApplication alloc] init] launch];
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+    [[[XCUIApplication alloc] init] terminate];
 }
 
-- (void)testExample {
+- (void)testValid {
+  
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.buttons[@"New Safety Concern"] tap];
+    
+    XCUIElementQuery *tablesQuery = app.tables;
+    XCUIElement *nameTextField = tablesQuery.textFields[@"First and Last Name"];
+    [nameTextField tap];
+    
+    [nameTextField typeText:@"Test Tan"];
+    
+    XCUIElement *phoneNumberTextField = tablesQuery.textFields[@"Phone Number"];
+    [phoneNumberTextField tap];
+    
+    XCUIElement *moreNumbersKey = app.keys[@"more, numbers"];
+    [moreNumbersKey tap];
+    [phoneNumberTextField typeText:@"3062912761"];
+    XCUIElement *emailTextField = tablesQuery.textFields[@"Email Address"];
+    [emailTextField tap];
+    [emailTextField typeText:@"Fake"];
+    [moreNumbersKey tap];
+    emailTextField = tablesQuery.textFields[@"Fake"];
+    [emailTextField typeText:@"@"];
+    
+    XCUIElement *moreLettersKey = app.keys[@"more, letters"];
+    [moreLettersKey tap];
+    emailTextField = tablesQuery.textFields[@"Fake@"];
+    [emailTextField typeText:@"no"];
+    [tablesQuery.staticTexts[@"Nature of Concern"] tap];
+    [tablesQuery.staticTexts[@"Category Five"] tap];
+    // Failed to find matching element please file bug (bugreport.apple.com) and provide output from Console.app
+    [tablesQuery.staticTexts[@"Facility"] tap];
+    [tablesQuery.staticTexts[@"Preston Extendicare"] tap];
+    [tablesQuery.textFields[@"Room Number"] tap];
+    [moreNumbersKey tap];
+    XCUIElement *roomTextField = tablesQuery.textFields[@"Room Number"];
+    [roomTextField typeText:@"45"];
+    
+    XCUIElement *textView = [[[tablesQuery childrenMatchingType:XCUIElementTypeCell] elementBoundByIndex:6] childrenMatchingType:XCUIElementTypeTextView].element;
+    [textView tap];
+    [textView typeText:@"None"];
+    //[[[[[app childrenMatchingType:XCUIElementTypeWindow] elementBoundByIndex:0] childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element tap];
+    [tablesQuery.staticTexts[@"Submit Concern"] tap];
+    
+    
     // Use recording to get started writing UI tests.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
 }
+
+- (void)testNullEmailPhone {
+    
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.buttons[@"New Safety Concern"] tap];
+    
+    XCUIElementQuery *tablesQuery = app.tables;
+    XCUIElement *nameTextField = tablesQuery.textFields[@"First and Last Name"];
+    [nameTextField tap];
+    
+    [nameTextField typeText:@"Test Tan"];
+    
+    
+    XCUIElement *moreNumbersKey = app.keys[@"more, numbers"];
+    
+    [tablesQuery.staticTexts[@"Nature of Concern"] tap];
+    [tablesQuery.staticTexts[@"Category Five"] tap];
+    // Failed to find matching element please file bug (bugreport.apple.com) and provide output from Console.app
+    [tablesQuery.staticTexts[@"Facility"] tap];
+    [tablesQuery.staticTexts[@"Preston Extendicare"] tap];
+    [tablesQuery.textFields[@"Room Number"] tap];
+    [moreNumbersKey tap];
+    XCUIElement *roomTextField = tablesQuery.textFields[@"Room Number"];
+    [roomTextField typeText:@"45"];
+    
+    XCUIElement *textView = [[[tablesQuery childrenMatchingType:XCUIElementTypeCell] elementBoundByIndex:6] childrenMatchingType:XCUIElementTypeTextView].element;
+    [textView tap];
+    [textView typeText:@"None"];
+    //[[[[[app childrenMatchingType:XCUIElementTypeWindow] elementBoundByIndex:0] childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element tap];
+    [tablesQuery.staticTexts[@"Submit Concern"] tap];
+    
+    
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    XCTAssert(app.alerts.element.staticTexts[@"Either a phone number or an email address must be provided when submitting a concern." ].exists);
+}
+
+- (void)testNullName {
+    
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.buttons[@"New Safety Concern"] tap];
+    
+    XCUIElementQuery *tablesQuery = app.tables;
+    
+    
+    XCUIElement *phoneNumberTextField = tablesQuery.textFields[@"Phone Number"];
+    [phoneNumberTextField tap];
+    
+    XCUIElement *moreNumbersKey = app.keys[@"more, numbers"];
+    [moreNumbersKey tap];
+    [phoneNumberTextField typeText:@"3062912761"];
+    XCUIElement *emailTextField = tablesQuery.textFields[@"Email Address"];
+    [emailTextField tap];
+    [emailTextField typeText:@"Fake"];
+    [moreNumbersKey tap];
+    emailTextField = tablesQuery.textFields[@"Fake"];
+    [emailTextField typeText:@"@"];
+    
+    XCUIElement *moreLettersKey = app.keys[@"more, letters"];
+    [moreLettersKey tap];
+    emailTextField = tablesQuery.textFields[@"Fake@"];
+    [emailTextField typeText:@"no"];
+    
+    [tablesQuery.staticTexts[@"Nature of Concern"] tap];
+    [tablesQuery.staticTexts[@"Category Five"] tap];
+    // Failed to find matching element please file bug (bugreport.apple.com) and provide output from Console.app
+    [tablesQuery.staticTexts[@"Facility"] tap];
+    [tablesQuery.staticTexts[@"Preston Extendicare"] tap];
+    [tablesQuery.textFields[@"Room Number"] tap];
+    [moreNumbersKey tap];
+    XCUIElement *roomTextField = tablesQuery.textFields[@"Room Number"];
+    [roomTextField typeText:@"45"];
+    
+    XCUIElement *textView = [[[tablesQuery childrenMatchingType:XCUIElementTypeCell] elementBoundByIndex:6] childrenMatchingType:XCUIElementTypeTextView].element;
+    [textView tap];
+    [textView typeText:@"None"];
+    //[[[[[app childrenMatchingType:XCUIElementTypeWindow] elementBoundByIndex:0] childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element tap];
+    [tablesQuery.staticTexts[@"Submit Concern"] tap];
+    
+    
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    XCTAssert(app.alerts.element.staticTexts[@"A first and last name must be provided when submitting a concern." ].exists);
+}
+
+- (void)testNullEmail {
+    
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.buttons[@"New Safety Concern"] tap];
+    
+    XCUIElementQuery *tablesQuery = app.tables;
+    XCUIElement *nameTextField = tablesQuery.textFields[@"First and Last Name"];
+    [nameTextField tap];
+    
+    [nameTextField typeText:@"Test Tan"];
+    
+    XCUIElement *phoneNumberTextField = tablesQuery.textFields[@"Phone Number"];
+    [phoneNumberTextField tap];
+    
+    XCUIElement *moreNumbersKey = app.keys[@"more, numbers"];
+    [moreNumbersKey tap];
+    [phoneNumberTextField typeText:@"3062912761"];
+    
+    [tablesQuery.staticTexts[@"Nature of Concern"] tap];
+    [tablesQuery.staticTexts[@"Category Five"] tap];
+    // Failed to find matching element please file bug (bugreport.apple.com) and provide output from Console.app
+    [tablesQuery.staticTexts[@"Facility"] tap];
+    [tablesQuery.staticTexts[@"Preston Extendicare"] tap];
+    [tablesQuery.textFields[@"Room Number"] tap];
+    [moreNumbersKey tap];
+    XCUIElement *roomTextField = tablesQuery.textFields[@"Room Number"];
+    [roomTextField typeText:@"45"];
+    
+    XCUIElement *textView = [[[tablesQuery childrenMatchingType:XCUIElementTypeCell] elementBoundByIndex:6] childrenMatchingType:XCUIElementTypeTextView].element;
+    [textView tap];
+    [textView typeText:@"None"];
+    //[[[[[app childrenMatchingType:XCUIElementTypeWindow] elementBoundByIndex:0] childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element tap];
+    [tablesQuery.staticTexts[@"Submit Concern"] tap];
+    
+    
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testNullPhone {
+    
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.buttons[@"New Safety Concern"] tap];
+    
+    XCUIElementQuery *tablesQuery = app.tables;
+    XCUIElement *nameTextField = tablesQuery.textFields[@"First and Last Name"];
+    [nameTextField tap];
+    
+    [nameTextField typeText:@"Test Tan"];
+
+    
+    XCUIElement *moreNumbersKey = app.keys[@"more, numbers"];
+        XCUIElement *emailTextField = tablesQuery.textFields[@"Email Address"];
+    [emailTextField tap];
+    [emailTextField typeText:@"Fake"];
+    [moreNumbersKey tap];
+    emailTextField = tablesQuery.textFields[@"Fake"];
+    [emailTextField typeText:@"@"];
+    
+    XCUIElement *moreLettersKey = app.keys[@"more, letters"];
+    [moreLettersKey tap];
+    emailTextField = tablesQuery.textFields[@"Fake@"];
+    [emailTextField typeText:@"no"];
+    
+    [tablesQuery.staticTexts[@"Nature of Concern"] tap];
+    [tablesQuery.staticTexts[@"Category Five"] tap];
+    // Failed to find matching element please file bug (bugreport.apple.com) and provide output from Console.app
+    [tablesQuery.staticTexts[@"Facility"] tap];
+    [tablesQuery.staticTexts[@"Preston Extendicare"] tap];
+    [tablesQuery.textFields[@"Room Number"] tap];
+    [moreNumbersKey tap];
+    XCUIElement *roomTextField = tablesQuery.textFields[@"Room Number"];
+    [roomTextField typeText:@"45"];
+    
+    XCUIElement *textView = [[[tablesQuery childrenMatchingType:XCUIElementTypeCell] elementBoundByIndex:6] childrenMatchingType:XCUIElementTypeTextView].element;
+    [textView tap];
+    [textView typeText:@"None"];
+    //[[[[[app childrenMatchingType:XCUIElementTypeWindow] elementBoundByIndex:0] childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element tap];
+    [tablesQuery.staticTexts[@"Submit Concern"] tap];
+    
+    
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testNullConcern {
+    
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.buttons[@"New Safety Concern"] tap];
+    
+    XCUIElementQuery *tablesQuery = app.tables;
+    XCUIElement *nameTextField = tablesQuery.textFields[@"First and Last Name"];
+    [nameTextField tap];
+    
+    [nameTextField typeText:@"Test Tan"];
+    
+    XCUIElement *phoneNumberTextField = tablesQuery.textFields[@"Phone Number"];
+    [phoneNumberTextField tap];
+    
+    XCUIElement *moreNumbersKey = app.keys[@"more, numbers"];
+    [moreNumbersKey tap];
+    [phoneNumberTextField typeText:@"3062912761"];
+    XCUIElement *emailTextField = tablesQuery.textFields[@"Email Address"];
+    [emailTextField tap];
+    [emailTextField typeText:@"Fake"];
+    [moreNumbersKey tap];
+    emailTextField = tablesQuery.textFields[@"Fake"];
+    [emailTextField typeText:@"@"];
+    
+    XCUIElement *moreLettersKey = app.keys[@"more, letters"];
+    [moreLettersKey tap];
+    emailTextField = tablesQuery.textFields[@"Fake@"];
+    [emailTextField typeText:@"no"];
+    // Failed to find matching element please file bug (bugreport.apple.com) and provide output from Console.app
+    [tablesQuery.staticTexts[@"Facility"] tap];
+    [tablesQuery.staticTexts[@"Preston Extendicare"] tap];
+    [tablesQuery.textFields[@"Room Number"] tap];
+    [moreNumbersKey tap];
+    XCUIElement *roomTextField = tablesQuery.textFields[@"Room Number"];
+    [roomTextField typeText:@"45"];
+    
+    XCUIElement *textView = [[[tablesQuery childrenMatchingType:XCUIElementTypeCell] elementBoundByIndex:6] childrenMatchingType:XCUIElementTypeTextView].element;
+    [textView tap];
+    [textView typeText:@"None"];
+    //[[[[[app childrenMatchingType:XCUIElementTypeWindow] elementBoundByIndex:0] childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element tap];
+    [tablesQuery.staticTexts[@"Submit Concern"] tap];
+    
+    
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(app.alerts.element.staticTexts[@"The nature of the concern must be specified when a concern is submitted" ].exists);
+}
+
+- (void)testNullFacility {
+    
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.buttons[@"New Safety Concern"] tap];
+    
+    XCUIElementQuery *tablesQuery = app.tables;
+    XCUIElement *nameTextField = tablesQuery.textFields[@"First and Last Name"];
+    [nameTextField tap];
+    
+    [nameTextField typeText:@"Test Tan"];
+    
+    XCUIElement *phoneNumberTextField = tablesQuery.textFields[@"Phone Number"];
+    [phoneNumberTextField tap];
+    
+    XCUIElement *moreNumbersKey = app.keys[@"more, numbers"];
+    [moreNumbersKey tap];
+    [phoneNumberTextField typeText:@"3062912761"];
+    XCUIElement *emailTextField = tablesQuery.textFields[@"Email Address"];
+    [emailTextField tap];
+    [emailTextField typeText:@"Fake"];
+    [moreNumbersKey tap];
+    emailTextField = tablesQuery.textFields[@"Fake"];
+    [emailTextField typeText:@"@"];
+    
+    XCUIElement *moreLettersKey = app.keys[@"more, letters"];
+    [moreLettersKey tap];
+    emailTextField = tablesQuery.textFields[@"Fake@"];
+    [emailTextField typeText:@"no"];
+    [tablesQuery.staticTexts[@"Nature of Concern"] tap];
+    [tablesQuery.staticTexts[@"Category Five"] tap];
+    // Failed to find matching element please file bug (bugreport.apple.com) and provide output from Console.app
+    [tablesQuery.textFields[@"Room Number"] tap];
+    [moreNumbersKey tap];
+    XCUIElement *roomTextField = tablesQuery.textFields[@"Room Number"];
+    [roomTextField typeText:@"45"];
+    
+    XCUIElement *textView = [[[tablesQuery childrenMatchingType:XCUIElementTypeCell] elementBoundByIndex:6] childrenMatchingType:XCUIElementTypeTextView].element;
+    [textView tap];
+    [textView typeText:@"None"];
+    //[[[[[app childrenMatchingType:XCUIElementTypeWindow] elementBoundByIndex:0] childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element tap];
+    [tablesQuery.staticTexts[@"Submit Concern"] tap];
+    
+    
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    XCTAssert(app.alerts.element.staticTexts[@"A facility name must be provided when submitting a concern." ].exists);
+}
+
+- (void)testNullRoom {
+    
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.buttons[@"New Safety Concern"] tap];
+    
+    XCUIElementQuery *tablesQuery = app.tables;
+    XCUIElement *nameTextField = tablesQuery.textFields[@"First and Last Name"];
+    [nameTextField tap];
+    
+    [nameTextField typeText:@"Test Tan"];
+    
+    XCUIElement *phoneNumberTextField = tablesQuery.textFields[@"Phone Number"];
+    [phoneNumberTextField tap];
+    
+    XCUIElement *moreNumbersKey = app.keys[@"more, numbers"];
+    [moreNumbersKey tap];
+    [phoneNumberTextField typeText:@"3062912761"];
+    XCUIElement *emailTextField = tablesQuery.textFields[@"Email Address"];
+    [emailTextField tap];
+    [emailTextField typeText:@"Fake"];
+    [moreNumbersKey tap];
+    emailTextField = tablesQuery.textFields[@"Fake"];
+    [emailTextField typeText:@"@"];
+    
+    XCUIElement *moreLettersKey = app.keys[@"more, letters"];
+    [moreLettersKey tap];
+    emailTextField = tablesQuery.textFields[@"Fake@"];
+    [emailTextField typeText:@"no"];
+    [tablesQuery.staticTexts[@"Nature of Concern"] tap];
+    [tablesQuery.staticTexts[@"Category Five"] tap];
+    // Failed to find matching element please file bug (bugreport.apple.com) and provide output from Console.app
+    [tablesQuery.staticTexts[@"Facility"] tap];
+    [tablesQuery.staticTexts[@"Preston Extendicare"] tap];
+    [tablesQuery.textFields[@"Room Number"] tap];
+    [moreNumbersKey tap];
+    
+    XCUIElement *textView = [[[tablesQuery childrenMatchingType:XCUIElementTypeCell] elementBoundByIndex:6] childrenMatchingType:XCUIElementTypeTextView].element;
+    [textView tap];
+    [textView typeText:@"None"];
+    //[[[[[app childrenMatchingType:XCUIElementTypeWindow] elementBoundByIndex:0] childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element tap];
+    [tablesQuery.staticTexts[@"Submit Concern"] tap];
+    
+    
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testNullActions {
+    
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app.buttons[@"New Safety Concern"] tap];
+    
+    XCUIElementQuery *tablesQuery = app.tables;
+    XCUIElement *nameTextField = tablesQuery.textFields[@"First and Last Name"];
+    [nameTextField tap];
+    
+    [nameTextField typeText:@"Test Tan"];
+    
+    XCUIElement *phoneNumberTextField = tablesQuery.textFields[@"Phone Number"];
+    [phoneNumberTextField tap];
+    
+    XCUIElement *moreNumbersKey = app.keys[@"more, numbers"];
+    [moreNumbersKey tap];
+    [phoneNumberTextField typeText:@"3062912761"];
+    XCUIElement *emailTextField = tablesQuery.textFields[@"Email Address"];
+    [emailTextField tap];
+    [emailTextField typeText:@"Fake"];
+    [moreNumbersKey tap];
+    emailTextField = tablesQuery.textFields[@"Fake"];
+    [emailTextField typeText:@"@"];
+    
+    XCUIElement *moreLettersKey = app.keys[@"more, letters"];
+    [moreLettersKey tap];
+    emailTextField = tablesQuery.textFields[@"Fake@"];
+    [emailTextField typeText:@"no"];
+    [tablesQuery.staticTexts[@"Nature of Concern"] tap];
+    [tablesQuery.staticTexts[@"Category Five"] tap];
+    // Failed to find matching element please file bug (bugreport.apple.com) and provide output from Console.app
+    [tablesQuery.staticTexts[@"Facility"] tap];
+    [tablesQuery.staticTexts[@"Preston Extendicare"] tap];
+    [tablesQuery.textFields[@"Room Number"] tap];
+    [moreNumbersKey tap];
+    XCUIElement *roomTextField = tablesQuery.textFields[@"Room Number"];
+    [roomTextField typeText:@"45"];
+    
+    //[[[[[app childrenMatchingType:XCUIElementTypeWindow] elementBoundByIndex:0] childrenMatchingType:XCUIElementTypeOther].element childrenMatchingType:XCUIElementTypeOther].element tap];
+    [tablesQuery.staticTexts[@"Submit Concern"] tap];
+    
+    
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
 
 @end
