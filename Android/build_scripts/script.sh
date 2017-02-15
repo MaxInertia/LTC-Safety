ae4d4d7eae1f806fa225f069fd4bd89c19a61993
Dorian Thiessen <NonInertial.git@usask.ca>
2017-02-14 20:56:14 -0600

    Fix nested conditional syntax

--- Android/build_scripts/script.sh
+++ Android/build_scripts/script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if [ "${TRAVIS_BRANCH}" == "android-app" ] ; then 
+if [[ "${TRAVIS_BRANCH}" == "android-app" ]] ; then 
 	cd Android/LTC-Safety && chmod +x gradlew && ./gradlew build -x test
-else if [ "${TRAVIS_BRANCH}" == "android-app-build" ] ; then 
-	cd Android/LTC-Safety && chmod +x gradlew && android list target &&./gradlew --continue test connectedAndroidTest
+else 
+	if [[ "${TRAVIS_BRANCH}" == "android-app-build" ]] ; then 
+		cd Android/LTC-Safety && chmod +x gradlew && android list target &&./gradlew --continue test connectedAndroidTest
+	fi
 fi
