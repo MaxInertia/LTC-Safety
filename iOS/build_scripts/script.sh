74f47912b7519bf3a2a32e1f12c19e64c4834231
Dorian Thiessen <NonInertial.git@usask.ca>
2017-02-21 17:09:27 -0600

    Modify xctool command flags

--- iOS/build_scripts/script.sh
+++ iOS/build_scripts/script.sh
@@ -6,6 +6,6 @@ if [ "${TRAVIS_BRANCH}" == "ios-app" ] ; then
 	xcodebuild -workspace iOS/LTC\ Safety/LTC\ Safety.xcworkspace -scheme LTC\ Safety build-for-testing CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
 else if [ "${TRAVIS_BRANCH}" == "ios-app-build" ] ; then 
 	xcodebuild -workspace iOS/LTC\ Safety/LTC\ Safety.xcworkspace -scheme LTC\ Safety build-for-testing CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
-	xctool -workspace iOS/LTC\ Safety/LTC\ Safety.xcworkspace -scheme LTC\ Safety -sdk iphonesimulator test
+	xctool -workspace iOS/LTC\ Safety/LTC\ Safety.xcworkspace -scheme LTC\ Safety -test-sdk iphonesimulator run-tests
 fi
 fi
