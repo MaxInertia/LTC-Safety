737773a2d23d2ea69f13555e38dcc64e4ff8ffd1
Dorian Thiessen <NonInertial.git@usask.ca>
2017-02-21 16:18:09 -0600

    Fix syntax in build script

--- iOS/build_scripts/script.sh
+++ iOS/build_scripts/script.sh
@@ -8,3 +8,4 @@ else if [ "${TRAVIS_BRANCH}" == "ios-app-build" ] ; then
 	xcodebuild -workspace iOS/LTC\ Safety/LTC\ Safety.xcworkspace -scheme LTC\ Safety build-for-testing
 	xctool -workspace iOS/LTC\ Safety/LTC\ Safety.xcworkspace -scheme LTC\ Safety -sdk iphonesimulator test
 fi
+fi
