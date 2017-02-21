cb38e649241589475a468f45e49f5ad3c7fab819
Allan Kerr <personal.allankerr@gmail.com>
2017-02-21 01:11:25 -0600

    Added basic template for remote push notifications.

--- iOS/LTC Safety/LTC Safety/AppDelegate.m
+++ iOS/LTC Safety/LTC Safety/AppDelegate.m
@@ -11,8 +11,11 @@
 #import "LTCConcernViewController.h"
 #import "LTCConcernDetailViewController.h"
 #import "LTCPersistentContainer.h"
+#import <UserNotifications/UserNotifications.h>
+@import Firebase;
+@import FirebaseMessaging;
 
-@interface AppDelegate () <UISplitViewControllerDelegate>
+@interface AppDelegate () <UISplitViewControllerDelegate, UNUserNotificationCenterDelegate>
 
 /**
  The persistent container that manages the application-wide managed object context used for loading and saving concerns.
@@ -26,6 +29,11 @@
  Performs setup for the application. This method is responsible for setting up the persistence container, app-wide navigation bar appearance and initializing the split view controller.
  */
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    // Observe the notification that is sent when the push notification token changes
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_tokenDidRefresh:) name:kFIRInstanceIDTokenRefreshNotification object:nil];
+    
+    [FIRApp configure];
     
     // Set up application wide persistence container and managed object context
     self.persistentContainer = [[LTCPersistentContainer alloc] initWithName:@"LTC_Safety"];
@@ -54,7 +62,75 @@
     
     controller.viewModel = [[LTCConcernViewModel alloc] initWithContext:self.persistentContainer.viewContext];
     
+    [self _registerForPushNotifications];
+    
     return YES;
+}
+
+- (void)_registerForPushNotifications {
+    
+    // iOS 9 or earlier
+    if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_9_x_Max) {
+        
+        UIUserNotificationType allNotificationTypes = (UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge);
+        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:allNotificationTypes categories:nil];
+        [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+        
+    } else {
+        // iOS 10 or later
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+        UNAuthorizationOptions authOptions = UNAuthorizationOptionAlert | UNAuthorizationOptionSound | UNAuthorizationOptionBadge;
+        
+        [[UNUserNotificationCenter currentNotificationCenter] requestAuthorizationWithOptions:authOptions completionHandler:^(BOOL granted, NSError * _Nullable error) {
+            if (error) {
+                // TODO Add logging
+            }
+        }];
+        [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+#endif
+    }
+    [[UIApplication sharedApplication] registerForRemoteNotifications];
+}
+
+- (void)_tokenDidRefresh:(NSNotificationCenter *)notification {
+    
+    NSString *token = [[FIRInstanceID instanceID] token];
+    
+    // Send the token to the backend to update the concern
+    
+    [self _connectToFirebaseMessaging];
+}
+
+- (void)_connectToFirebaseMessaging {
+    
+    [[FIRMessaging messaging] connectWithCompletion:^(NSError *error){
+        if (error) {
+            // TODO Add logging
+        }
+    }];
+}
+
+- (void)_disconnectFromFirebaseMessaging {
+    [[FIRMessaging messaging] disconnect];
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+        [[FIRInstanceID instanceID] setAPNSToken:deviceToken type:FIRInstanceIDAPNSTokenTypeSandbox];
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    [self _connectToFirebaseMessaging];
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    [self _disconnectFromFirebaseMessaging];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+    
+    // TODO Handle push notification
+    
+    completionHandler(UIBackgroundFetchResultNewData);
 }
 
 /**
