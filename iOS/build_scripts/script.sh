5a3223a18207cae2fbefb4cd2f7db4a265020860
Dorian Thiessen <NonInertial.git@usask.ca>
2017-02-21 19:33:32 -0600

    Reintroduce xctool. xcodebuild: build-for-test then xctool run-tests

--- iOS/build_scripts/script.sh
+++ iOS/build_scripts/script.sh
@@ -3,19 +3,25 @@
 set -e
 
 if [ "${TRAVIS_BRANCH}" == "ios-app" ] ; then 
-	xcodebuild -workspace iOS/LTC\ Safety/LTC\ Safety.xcworkspace -scheme LTC\ Safety -sdk iphonesimulator build-for-testing CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
+	xcodebuild \
+	-workspace iOS/LTC\ Safety/LTC\ Safety.xcworkspace \
+	-scheme LTC\ Safety \
+	-sdk iphonesimulator \
+	build \
+	CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
+	
 else if [ "${TRAVIS_BRANCH}" == "ios-app-build" ] ; then 
 	xcodebuild \
 	-workspace iOS/LTC\ Safety/LTC\ Safety.xcworkspace \
 	-scheme LTC\ Safety \
 	-sdk iphonesimulator \
-	test \
+	build-for-tests \
 	CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
 	
-	#xctool \
-	#-workspace iOS/LTC\ Safety/LTC\ Safety.xcworkspace \
-	#-scheme LTC\ Safety \
-	#-sdk iphonesimulator \
-	#run-tests
+	xctool \
+	-workspace iOS/LTC\ Safety/LTC\ Safety.xcworkspace \
+	-scheme LTC\ Safety \
+	-sdk iphonesimulator \
+	run-tests
 fi
 fi
