eb49cf5b2670e550431faef61d2b7ca87680fc2d
Dorian Thiessen <NonInertial.git@usask.ca>
2017-02-21 18:48:55 -0600

    Try running tests with 'xcodebuild'

--- iOS/build_scripts/script.sh
+++ iOS/build_scripts/script.sh
@@ -5,7 +5,17 @@ set -e
 if [ "${TRAVIS_BRANCH}" == "ios-app" ] ; then 
 	xcodebuild -workspace iOS/LTC\ Safety/LTC\ Safety.xcworkspace -scheme LTC\ Safety -sdk iphonesimulator build-for-testing CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
 else if [ "${TRAVIS_BRANCH}" == "ios-app-build" ] ; then 
-	xcodebuild -workspace iOS/LTC\ Safety/LTC\ Safety.xcworkspace -scheme LTC\ Safety -sdk iphonesimulator build-for-testing CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
-	xctool -workspace iOS/LTC\ Safety/LTC\ Safety.xcworkspace -scheme LTC\ Safety -sdk iphonesimulator run-tests
+	xcodebuild \
+	-workspace iOS/LTC\ Safety/LTC\ Safety.xcworkspace \
+	-scheme LTC\ Safety \
+	-sdk iphonesimulator \
+	test \
+	CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
+	
+	#xctool \
+	#-workspace iOS/LTC\ Safety/LTC\ Safety.xcworkspace \
+	#-scheme LTC\ Safety \
+	#-sdk iphonesimulator \
+	#run-tests
 fi
 fi
