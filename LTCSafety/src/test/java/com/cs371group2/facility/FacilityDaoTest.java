b55d8d43d9f8fdb40338aa6d2543fca1ed5b407d
bmj920 <bmj920@usask.ca>
2017-02-24 19:26:12 -0600

    Created unit testing for FacilityDao class
    Created unit testing for the FacilityDao class to ensure proper functionality

---
+++ LTCSafety/src/test/java/com/cs371group2/facility/FacilityDaoTest.java
@@ -0,0 +1,12 @@
+package com.cs371group2.facility;
+
+import com.cs371group2.DatastoreTest;
+
+/**
+ * Created by Brandon on 2017-02-24.
+ */
+public class FacilityDaoClass extends DatastoreTest {
+
+
+
+}
