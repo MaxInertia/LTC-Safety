59871a4117eef0533337e981fa2148fe5d764188
Dorian Thiessen <NonInertial.git@usask.ca>
2017-03-19 21:06:25 -0600

    Modify android build scripts

--- Android/build_scripts/script.sh
+++ Android/build_scripts/script.sh
@@ -3,12 +3,6 @@
 # Move to project directory, allow execution of gradle wrapper
 cd Android/LTC-Safety && chmod +x gradlew
 
-if [ "${TRAVIS_BRANCH}" == "android-app" ] ; then 
-	# Build, run no Tests
-	./gradlew build -x test 
-else 
-	if [ "${TRAVIS_BRANCH}" == "android-app-build" ] ; then 
-		# Run all JVM and Instrumented Unit Tests
-		android list target && ./gradlew --continue test connectedAndroidTest
-	fi
-fi
+# Run all JVM and Instrumented Unit Tests
+android list target && ./gradlew --continue test connectedAndroidTest
+	
