bc42a4cfa8cd7883682b2308e3533096c9b2d6d9
Dorian Thiessen <NonInertial.git@usask.ca>
2017-04-03 21:23:26 -0600

    Modify test command (use sdk from console output)

--- iOS/build_scripts/script.sh
+++ iOS/build_scripts/script.sh
@@ -14,8 +14,7 @@ else if [ "${TRAVIS_BRANCH}" == "ios-app-build" ] ; then
 	xcodebuild \
 	-workspace iOS/LTC\ Safety/LTC\ Safety.xcworkspace \
 	-scheme LTC\ Safety \
-	-sdk iphonesimulator \
-	-destination 'name=iPhone SE' \
+	-destination 'platform:iOS Simulator, id:DB794781-65A7-4884-8D00-AAC3CBD39A44, OS:10.2, name:iPhone SE' \
 	test
 
 	#xcodebuild \
