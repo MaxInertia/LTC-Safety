0ef8c59e438357cbccdefe52807f48c7b6fa3c94
Dorian Thiessen <dorian.thiessen@usask.ca>
2017-07-31 16:33:10 -0600

    Revert to android-18 for emulator, add compile sdk

--- Android/build_scripts/beforeScript.sh
+++ Android/build_scripts/beforeScript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Boot up the emulator for the Instrumented Tests
-echo no | android create avd --force -n test -t android-25 --abi armeabi-v7a
+echo no | android create avd --force -n test -t android-18 --abi armeabi-v7a
 emulator -avd test -no-skin -no-audio -no-window &
 android-wait-for-emulator
 adb shell input keyevent 82 &
